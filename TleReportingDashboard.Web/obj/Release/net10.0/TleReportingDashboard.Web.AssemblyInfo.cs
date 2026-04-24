39e1fbb401b9766abd2c3093d6fd5066620190f8
Rob Minter <rob.minter@ralisservices.com>
2026-04-23 20:29:26 -0700

    Updated to phase 4

--- TleReportingDashboard.Web/obj/Release/net10.0/TleReportingDashboard.Web.AssemblyInfo.cs
+++ TleReportingDashboard.Web/obj/Release/net10.0/TleReportingDashboard.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TleReportingDashboard.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+429c3c8b3dece1922b327b3211c68a1d87af4021")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5022ad0ba0e63743c475d3db4a1c00448c39956")]
 [assembly: System.Reflection.AssemblyProductAttribute("TleReportingDashboard.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TleReportingDashboard.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
